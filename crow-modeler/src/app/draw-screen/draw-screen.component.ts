54f451bc335f513f0baf6ad92a1e66c68d2c6e6f
JanSievers <jan-thorben.sievers@stud.hs-bochum.de>
2024-05-19 19:53:42 +0200

    Header eingefügt

--- crow-modeler/src/app/draw-screen/draw-screen.component.ts
+++ crow-modeler/src/app/draw-screen/draw-screen.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { GojsAngularModule } from 'gojs-angular';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { GojsDiagramComponent } from '../gojs-diagram/gojs-diagram.component';
+import { HeadComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-draw-screen',
   standalone: true,
-  imports: [GojsAngularModule, GojsDiagramComponent, MatSidenavModule],
+  imports: [GojsAngularModule, GojsDiagramComponent, MatSidenavModule, HeadComponent],
   templateUrl: './draw-screen.component.html',
   styleUrl: './draw-screen.component.css'
   // encapsulation: ViewEncapsulation.None
---
+++ crow-modeler/src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-head',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeadComponent {
+
+}
